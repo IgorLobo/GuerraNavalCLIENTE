ca1bf66c0ceff43e8a20c09723fcc75e68666f09
xHygor <hygordebarross@gmail.com>
2018-10-15 16:33:40 -0300

    implatação da conexao client servidor

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,77 @@
 package application;
-	
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 import connection.Client;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
-	static Client client = null;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		try{
-			launch(args);
-			client =  new Client(5555);		
-		}catch (Exception e) {
-			// TODO: handle exception
-		}		
-		
+		//launch(args);
+		try {
+			final Socket cliente = new Socket("localhost", 9999);
+
+			// lendo mensagem do servidor
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+						while (true) {
+							/*String msg = leitor.readLine();
+							System.out.println("O servidor disse: teste" + msg);*/
+						}
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(null, "Impossivel ler a mensagem do servidor", "ERRO",
+								JOptionPane.ERROR_MESSAGE);
+						e.printStackTrace();
+					}
+				}
+			}.start();
+
+			
+			// escrevendo para o servidor (sync)
+			PrintWriter escritor = new PrintWriter(cliente.getOutputStream(),true);
+			BufferedReader leitorTerminal = new BufferedReader(new InputStreamReader(System.in));
+			while (true) {
+				
+				
+				/*String mensagemTerminal = leitorTerminal.readLine();
+				escritor.println(mensagemTerminal);*/
+			}
+			
+		} catch (UnknownHostException e) {
+			JOptionPane.showMessageDialog(null, "O endereço informado é inválido", "ERRO", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "O servidor pode estar inacesível", "ERRO", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/connection/Client.java
+++ src/connection/Client.java
@@ -5,29 +5,50 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.spec.MGF1ParameterSpec;
 
 public class Client {
 	private Socket socket = null;
-	//private String endereço = "localhost";	
-	
-	public Client(int port){		
-		//this.endereço = endereço;		
+	// private String endereço = "localhost";
+
+	public Client(int port) {
+		// this.endereço = endereço;
 		try {
 			this.socket = new Socket("localhost", port);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	public void objectOutputStream(Socket socket) throws IOException {
-		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+	/*-----------Fechamento de conexão-----------*/
+	public void fecharSocket(Socket socket) throws IOException {
+		socket.close();
 	}
-	
-	public void objectInputStream(Socket socket) throws IOException {
-		ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+	/*--------Validação de chegada e sáida (protocolo)-------*/
+	public void tratamentoConexao(Socket socket) throws IOException {
+		try {
+			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+			String msg = "Opa";
+			output.writeUTF(msg);
+
+			msg = input.readUTF();
+			System.out.println("Resposta: " + msg);
+
+			// fecha entrada/saída de dadados
+			output.close();
+			input.close();
+		}
+
+		catch (IOException e) {
+			// tratamento de falhas
+		} finally {
+			fecharSocket(socket);
+		}
 	}
-	
-	
+
 }
